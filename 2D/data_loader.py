4586dd794d7979b9830662f837ce7e77c2c89528
Rtlyc <yuchenliu0513@gmail.com>
2022-12-05 16:05:02 -0500

    do not shuffle data and take only first 1000

--- 2D/data_loader.py
+++ 2D/data_loader.py
@@ -32,17 +32,17 @@ class data(object):
 
         # set seed
         random.seed(1)
-        random.shuffle(object_names)
+        # random.shuffle(object_names) #?
         if args.limit_data:
             object_names = object_names[:1000]
 
         for i, n in enumerate(tqdm(object_names)):
             if os.path.exists(VOXEL_LOCATION + n + ".npy"):
-                if set_type == "train" and int(n) < 23000:
+                if set_type == "train" and int(n) < 800: #? 23000
                     self.object_names.append([n, None])
-                if set_type == "valid" and int(n) >= 23000 and int(n) < 24500:
+                if set_type == "valid" and int(n) >= 800 and int(n) < 900: #? 23000, 24500
                     self.object_names.append([n, i])
-                if set_type == "test" and int(n) >= 24500:
+                if set_type == "test" and int(n) >= 900: #? 24500
                     self.object_names.append([n, i])
         print(f"The number of {set_type} set objects found : {len(self.object_names)}")
 
