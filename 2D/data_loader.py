93d2e7cec38e7a300791e353f989fca1e3e19eae
Rtlyc <yuchenliu0513@gmail.com>
2022-12-05 16:21:45 -0500

    sd

--- 2D/data_loader.py
+++ 2D/data_loader.py
@@ -39,6 +39,7 @@ class data(object):
         print(object_names)
         for i, n in enumerate(tqdm(object_names)):
             if os.path.exists(VOXEL_LOCATION + n + ".npy"):
+                print("*1"*10)
                 if set_type == "train" and int(n) < 800: #? 23000
                     self.object_names.append([n, None])
                 if set_type == "valid" and int(n) >= 800 and int(n) < 900: #? 23000, 24500
