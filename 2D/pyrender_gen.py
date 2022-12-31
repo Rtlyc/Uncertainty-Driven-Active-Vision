654e689e39e4258a7c16795dc5670a454870bf24
Rtlyc <yuchenliu0513@gmail.com>
2022-12-31 16:41:55 -0500

    add a dummy pyrender_gen file and a luomo object

---
+++ 2D/pyrender_gen.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.insert(0, "../")
+
+from utils import rendering
+from PIL import Image
+import trimesh
+
+renderer = rendering.Renderer([128, 128])
+
+# load the object
+object_path = "our_mesh/luomo.obj"
+mesh = trimesh.load(object_path)
+renderer.remove_objects()
+renderer.add_object(mesh)
+
+location = renderer.random_position(radius=1, num=1, seed=0)
+orientation = renderer.cam_from_positions(location)
+renderer.update_camera_pose(location, orientation)
+image = Image.fromarray(renderer.render())
+
+
+image.save('image.png')
+
+
