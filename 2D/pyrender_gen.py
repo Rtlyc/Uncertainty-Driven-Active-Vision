2ee3e662506f4c472b4cbb9a0e76e7e6a174d93a
Rtlyc <yuchenliu0513@gmail.com>
2023-01-01 15:43:51 -0500

    change location

--- 2D/pyrender_gen.py
+++ 2D/pyrender_gen.py
@@ -4,6 +4,7 @@ sys.path.insert(0, "../")
 from utils import rendering
 from PIL import Image
 import trimesh
+import numpy as np
 
 renderer = rendering.Renderer([128, 128])
 
@@ -14,7 +15,7 @@ renderer.remove_objects()
 renderer.add_object(mesh, add_faces=True)
 
 location = renderer.random_position(radius=5, num=1, seed=0)
-location = [0, 0, 5]
+location = np.array([0,0,5]).reshape(-1)
 orientation = renderer.cam_from_positions(location)
 renderer.update_camera_pose(location, orientation)
 image = Image.fromarray(renderer.render())
