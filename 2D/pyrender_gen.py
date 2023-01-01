ece3561128c2cab6c8708cf57bdf381ec58b5fc4
Rtlyc <yuchenliu0513@gmail.com>
2023-01-01 16:06:52 -0500

    change radius and scale

--- 2D/pyrender_gen.py
+++ 2D/pyrender_gen.py
@@ -11,14 +11,13 @@ renderer = rendering.Renderer([128, 128])
 # load the object
 object_path = "our_mesh/luomo.obj"
 mesh = trimesh.load(object_path)
-scale = 0.2
+scale = 0.02
 mesh.apply_scale([scale, scale, scale]) 
 renderer.remove_objects()
 renderer.add_object(mesh,  add_faces=True)
 
-location = renderer.random_position(radius=10, num=1, seed=2)
+location = renderer.random_position(radius=0.8, num=1, seed=2)
 print(location)
-# location = np.array([[5,0,0]])
 orientation = renderer.cam_from_positions(location)
 renderer.update_camera_pose(location, orientation)
 image = Image.fromarray(renderer.render())
