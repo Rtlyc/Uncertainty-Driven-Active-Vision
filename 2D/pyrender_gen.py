424aae12bb5edea07f8b86bf2f1cf05ec182d49d
Rtlyc <yuchenliu0513@gmail.com>
2023-01-01 15:45:16 -0500

    change location

--- 2D/pyrender_gen.py
+++ 2D/pyrender_gen.py
@@ -12,10 +12,10 @@ renderer = rendering.Renderer([128, 128])
 object_path = "our_mesh/luomo.obj"
 mesh = trimesh.load(object_path)
 renderer.remove_objects()
-renderer.add_object(mesh, add_faces=True)
+renderer.add_object(mesh, position=[0,0,5], add_faces=True)
 
 location = renderer.random_position(radius=5, num=1, seed=0)
-location = np.array([0,0,5]).reshape(-1)
+location = np.array([0,0,5])
 orientation = renderer.cam_from_positions(location)
 renderer.update_camera_pose(location, orientation)
 image = Image.fromarray(renderer.render())
