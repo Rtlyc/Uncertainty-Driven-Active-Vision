2d4715626f98e69138df2596f3db827ff0fac88a
Rtlyc <yuchenliu0513@gmail.com>
2023-01-01 15:42:13 -0500

    change location

--- 2D/pyrender_gen.py
+++ 2D/pyrender_gen.py
@@ -14,6 +14,7 @@ renderer.remove_objects()
 renderer.add_object(mesh, add_faces=True)
 
 location = renderer.random_position(radius=5, num=1, seed=0)
+location = [0, 0, 5]
 orientation = renderer.cam_from_positions(location)
 renderer.update_camera_pose(location, orientation)
 image = Image.fromarray(renderer.render())
