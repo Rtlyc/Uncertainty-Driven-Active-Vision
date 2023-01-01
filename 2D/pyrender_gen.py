b255db1fac4bac617823f3022035bcc2171bafb9
Rtlyc <yuchenliu0513@gmail.com>
2023-01-01 15:54:59 -0500

    change seed

--- 2D/pyrender_gen.py
+++ 2D/pyrender_gen.py
@@ -14,7 +14,7 @@ mesh = trimesh.load(object_path)
 renderer.remove_objects()
 renderer.add_object(mesh,  add_faces=True)
 
-location = renderer.random_position(radius=9, num=1, seed=0)
+location = renderer.random_position(radius=9, num=1, seed=1)
 print(location)
 # location = np.array([[5,0,0]])
 orientation = renderer.cam_from_positions(location)
