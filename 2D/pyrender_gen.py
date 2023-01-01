d16977d8c858df5282767c4b93bcd5c7f9003627
Rtlyc <yuchenliu0513@gmail.com>
2023-01-01 16:23:46 -0500

    gen dataset

--- 2D/pyrender_gen.py
+++ 2D/pyrender_gen.py
@@ -6,24 +6,74 @@ from PIL import Image
 import trimesh
 import numpy as np
 
-renderer = rendering.Renderer([128, 128])
+# renderer = rendering.Renderer([128, 128])
 
-# load the object
+# # load the object
+# object_path = "our_mesh/luomo.obj"
+# mesh = trimesh.load(object_path)
+# scale = 0.013
+# mesh.apply_scale([scale, scale, scale]) 
+# renderer.remove_objects()
+# renderer.add_object(mesh,  add_faces=True)
+
+# location = renderer.random_position(radius=0.8, num=1, seed=3)
+# print(location)
+# orientation = renderer.cam_from_positions(location)
+# renderer.update_camera_pose(location, orientation)
+# image = Image.fromarray(renderer.render())
+
+
+# image.save('image.png')
+
+import trimesh
+import pyrender
+import numpy as np
+
+# Load the mesh and scale it
 object_path = "our_mesh/luomo.obj"
 mesh = trimesh.load(object_path)
 scale = 0.013
-mesh.apply_scale([scale, scale, scale]) 
-renderer.remove_objects()
+mesh.apply_scale([scale, scale, scale])
+
+# Create a renderer and add the mesh
+renderer = pyrender.Renderer([128, 128])
 renderer.add_object(mesh,  add_faces=True)
 
-location = renderer.random_position(radius=0.8, num=1, seed=3)
-print(location)
-orientation = renderer.cam_from_positions(location)
-renderer.update_camera_pose(location, orientation)
-image = Image.fromarray(renderer.render())
+# Define the range of y and x rotations
+r = 0.8
+theta = 180  # Change this value to change the number of rotations
+y = 180 // theta
+x = 360 // theta
+idx = 0
+output_dir = "luomo_raw"
 
+# Loop over the rotations
+for y_rot in range(y):
+    y_mat_rot = pyrender.transformations.rotation_matrix(theta * y_rot, [0, 1, 0])
+    for x_rot in range(x):
+        x_mat_rot = pyrender.transformations.rotation_matrix(theta * x_rot, [1, 0, 0])
+        mat_rot = np.dot(y_mat_rot, x_mat_rot)
+        
+        # Update the camera location and orientation
+        camera_vec = np.array([0, 0, r])
+        camera_rot = np.array([0, 0, 0, 1])
+        camera_vec = np.dot(mat_rot, camera_vec)
+        camera_rot = np.dot(mat_rot, camera_rot)
+        renderer.update_camera_pose(camera_vec, camera_rot)
+        
+        # Render the scene and save the image
+        image = Image.fromarray(renderer.render())
+        img_name = f"{idx}.png"
+        image.save(os.path.join(output_dir, img_name))
+        
+        # Save the camera position and orientation
+        d = {}
+        d['position'] = np.array(camera_vec)
+        d['rotation'] = np.array(camera_rot)
+        npy_name = f"P_{idx}.npy"
+        np.save(os.path.join(output_dir, npy_name), d)
+        idx += 1
 
-image.save('image.png')
 
 
 
