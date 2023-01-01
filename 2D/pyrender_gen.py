8d33d1ffa908bba8e05ec82903458dabca780521
Rtlyc <yuchenliu0513@gmail.com>
2023-01-01 17:16:28 -0500

    try camera_rot

--- 2D/pyrender_gen.py
+++ 2D/pyrender_gen.py
@@ -71,6 +71,7 @@ for y_rot in range(y):
         camera_vec = np.dot(mat_rot, camera_vec)
         camera_rot = R.from_matrix(mat_rot)
         camera_rot = camera_rot.as_euler("xyz", degrees=True)
+        camera_rot = renderer.cam_from_positions(camera_vec)
         if DEBUG: 
             print(f"Camera position: {camera_vec}")
             print(f"Camera rotation: {camera_rot}")
