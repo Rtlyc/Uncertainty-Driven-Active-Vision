a8fb14adfe88962ec6862409c0226f3c0121c2e7
Rtlyc <yuchenliu0513@gmail.com>
2023-01-01 15:40:13 -0500

    change radius

--- 2D/pyrender_gen.py
+++ 2D/pyrender_gen.py
@@ -12,9 +12,8 @@ object_path = "our_mesh/luomo.obj"
 mesh = trimesh.load(object_path)
 renderer.remove_objects()
 renderer.add_object(mesh, add_faces=True)
-print(renderer.scene.get_nodes())
 
-location = renderer.random_position(radius=1, num=1, seed=0)
+location = renderer.random_position(radius=10, num=1, seed=0)
 orientation = renderer.cam_from_positions(location)
 renderer.update_camera_pose(location, orientation)
 image = Image.fromarray(renderer.render())
