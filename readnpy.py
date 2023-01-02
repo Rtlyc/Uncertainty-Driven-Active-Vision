c1cb626cd1597167d410794457f353e124870925
Rtlyc <yuchenliu0513@gmail.com>
2023-01-02 15:59:43 -0500

    1

--- readnpy.py
+++ readnpy.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-file1 = "luomo_blender/3/P_2.npy"
-file2 = "luomo/3/P_2.npy"
+file1 = "luomo_blender/3/P_3.npy"
+file2 = "luomo/3/P_3.npy"
 
 data1 = np.load(file1, allow_pickle=True)
 data2 = np.load(file2, allow_pickle=True)
