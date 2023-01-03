7f0a300783d7090739f8f935dcee0e9524952314
Rtlyc <yuchenliu0513@gmail.com>
2023-01-03 15:06:39 -0500

    evaluate pyrender

--- 2D/pyrender_gen.py
+++ 2D/pyrender_gen.py
@@ -44,21 +44,22 @@ renderer.add_object(mesh,  add_faces=True)
 
 # Define the range of y and x rotations
 r = 0.8
-theta = 10  # Change this value to change the number of rotations
+r = 1
+theta = 30  # Change this value to change the number of rotations
 y = 180 // theta
 x = 360 // theta
 idx = 0
 output_dir = "../luomo_raw"
 
 # Loop over the rotations
-for y_rot in range(y):
+for y_rot in range(1):
     # Create a rotation matrix for the y-axis
     y_mat_rot = np.array([
         [np.cos(np.deg2rad(theta * y_rot)), 0, np.sin(np.deg2rad(theta * y_rot))],
         [0, 1, 0],
         [-np.sin(np.deg2rad(theta * y_rot)), 0, np.cos(np.deg2rad(theta * y_rot))]
     ])
-    for x_rot in range(x):
+    for x_rot in range(2):
         # Create a rotation matrix for the x-axis
         x_mat_rot = np.array([
             [1, 0, 0],
@@ -92,18 +93,18 @@ for y_rot in range(y):
         renderer.update_camera_pose(camera_vec, camera_rot)
 
         
-        # Render the scene and save the image
-        image = Image.fromarray(renderer.render())
-        img_name = f"{idx}.png"
-        image.save(os.path.join(output_dir, img_name))
+        # # Render the scene and save the image
+        # image = Image.fromarray(renderer.render())
+        # img_name = f"{idx}.png"
+        # image.save(os.path.join(output_dir, img_name))
         
-        # Save the camera position and orientation
-        d = {}
-        d['position'] = np.array(camera_vec)
-        d['rotation'] = np.array(camera_rot)
-        npy_name = f"P_{idx}.npy"
-        np.save(os.path.join(output_dir, npy_name), d)
-        idx += 1
+        # # Save the camera position and orientation
+        # d = {}
+        # d['position'] = np.array(camera_vec)
+        # d['rotation'] = np.array(camera_rot)
+        # npy_name = f"P_{idx}.npy"
+        # np.save(os.path.join(output_dir, npy_name), d)
+        # idx += 1
 
 
 
